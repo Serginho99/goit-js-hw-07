3038a052f54865161c581c404ed09ffbd0bf36f5
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-12 14:46:39 +0300

    add task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+const wrapper = document.querySelector(".gallery");
+const fn = imgCreate(galleryItems);
+wrapper.insertAdjacentHTML("afterbegin", fn);
+
+wrapper.addEventListener("click", onEventWrapperClick);
+
+function onEventWrapperClick(event) {
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  event.preventDefault();
+  console.log(event.target);
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" alt="${event.target.alt}"></img>`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onEscPress);
+      },
+    }
+  );
+
+  function onEscPress(event) {
+    if (event.code === "Escape") {
+      console.log(event.code);
+      instance.close();
+      window.removeEventListener("keydown", onEscPress);
+    }
+  }
+
+  instance.show();
+}
+
+function imgCreate(galleryItems) {
+  const markup = galleryItems
+    .map(({ original, preview, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join(" ");
+  return markup;
+}
