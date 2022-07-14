75d2859046f27d76257f460b57da76e6db42ff83
Sergey Ostroverkh <ostroverkh27@gmail.com>
2022-07-14 13:59:26 +0300

    add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+const fn = imgCreate(galleryItems);
+listEl.insertAdjacentHTML("afterbegin", fn);
+listEl.addEventListener("click", onEventWrapperClick);
+
+function onEventWrapperClick(event) {
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  // if (event.target.nodeName !== "IMG") {
+  //   return;
+  // }
+  event.preventDefault();
+  console.log(event.target);
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+function imgCreate(galleryItems) {
+  const markup = galleryItems
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" data-caption=${description}/>
+</a>`;
+    })
+    .join(" ");
+  return markup;
+}
